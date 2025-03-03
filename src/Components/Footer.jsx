ea96938a5d9e2973b482441ee14767b21d173095
mohanveera9 <mohan103999@gmail.com>
2025-03-03 15:06:00 +0530

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,21 +1,25 @@
 import { FaInstagram, FaFacebookF, FaXTwitter, FaLinkedinIn } from "react-icons/fa6";
-import { MdEmail, MdPhone } from "react-icons/md";
+import { MdEmail, MdPhone , MdLocationOn } from "react-icons/md";
 
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-green-500 to-green-700 text-white p-8 mt-6 md:px-20">
+    <footer className="bg-gradient-to-r from-green-500 to-green-700 text-white p-8 mt-6 md:px-24">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between  md:items-start items-start md:text-left">
         
         {/* Contact Section */}
         <div className="mb-6 md:mb-0 md:w-1/2">
           <h2 className="text-2xl lg:text-3xl font-bold font-josefin">Contact Us</h2>
           <div className="flex items-center  md:justify-start gap-3 mt-3">
-            <MdPhone className="text-2xl text-black" />
+            <MdPhone className="text-2xl text-white" />
             <span className="font-normal lg:text-xl text-sm">+91- 9502774125</span>
           </div>
           <div className="flex items-center  md:justify-start gap-3 mt-2">
-            <MdEmail className="text-2xl text-black" />
+            <MdEmail className="text-2xl text-white" />
             <span className="font-normal lg:text-xl text-sm">locusofficial21@gmail.com</span>
+          </div>
+          <div className="flex items-center  md:justify-start gap-3 mt-2">
+            <MdLocationOn className="text-2xl text-white" />
+            <span className="font-normal lg:text-xl text-sm">Nuzvid, Eluru dist, 521202</span>
           </div>
         </div>
 
